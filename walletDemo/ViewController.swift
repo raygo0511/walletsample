4785fde0c5c829a79427b054784ecd84682a0e7c
rayshen <rayshen@redpay.io>
2020-03-25 12:09:18 +0800

    Add the Gomoblie4wallet

--- walletDemo/ViewController.swift
+++ walletDemo/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Gomobile4wallet
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //golib
+        let reply = Gomobile4walletGreetings("Demo test")
+        print("golib reply: \(reply)")
+        
+        //golib error
+        var err : NSError?
+        var num: Int = 0
+        Gomobile4walletNumberError(11, &num, &err)
+        print("golib num: \(num), err: \(err)")
     }
 
 
